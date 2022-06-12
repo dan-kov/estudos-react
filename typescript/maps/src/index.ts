450b9b884d9578e219135a0a4c10b06c6607f6fa
dan-kov <dkovalechyn@yahoo.com.br>
2022-06-12 10:23:10 -0300

    map use

--- typescript/maps/src/index.ts
+++ typescript/maps/src/index.ts
@@ -1,10 +1,13 @@
-import { User } from './User';
-import { Company } from './Company';
+//import { User } from './User';
+//import { Company } from './Company';
 
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+const mapDiv = document.getElementById('map') as HTMLElement;
+new google.maps.Map(mapDiv, {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
 
